4ab5ad83c30c04cab465be893c85d91994a6e06c
Varsha Panchal <varsha3051@gmail.com>
2022-01-19 16:45:21 -0500

    creat client and add into play

--- play.js
+++ play.js
@@ -1,17 +1,5 @@
+const {client} = require("./client");
+
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost', // IP address here,
-    port: 50541 // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
 console.log("Connecting ...");
-connect();
